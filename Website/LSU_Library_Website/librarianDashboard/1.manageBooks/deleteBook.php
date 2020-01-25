38b032cf4f5fd1403eb47855045c3efa3e5cab6c
Lucas Lahiru.H.H <hvlhasanka@students.nsbm.lk>
2020-01-25 10:50:50 +0530

    Phase 11.11
    Improvements to frontend and backend of the librarian dashboard.

--- Website/LSU_Library_Website/librarianDashboard/1.manageBooks/deleteBook.php
+++ Website/LSU_Library_Website/librarianDashboard/1.manageBooks/deleteBook.php
@@ -3,11 +3,6 @@
 
     $ISBN = $_GET['isbn'];
 
-    /* Deleting record from Book table */
-    $bookSQL = "DELETE FROM Book WHERE ISBN='$ISBN';";
-
-    $bookResult = mysqli_query($databaseConn, $bookSQL);
-
     /* Deleting record from BookAuthor table */
     $bookAuthorSQL = "DELETE FROM BookAuthor WHERE bISBN='$ISBN';";
 
@@ -18,39 +13,42 @@
 
     $reservedBookResult = mysqli_query($databaseConn, $reservedBookSQL);
 
+      /* Retrieving BorrowDetails ID from the Borrow table */
+      $borrowDetailsIDSQL = "SELECT bdBDID FROM Borrow WHERE bISBN = '$ISBN';";
+
+      $borrowDetailsIDResult = mysqli_query($databaseConn, $borrowDetailsIDSQL);
+
+      $borrowDetailsID = "";
+
+      while($borrowDetailsIDRow = mysqli_fetch_array($borrowDetailsIDResult)){
+
+        $borrowDetailsID = $borrowDetailsIDRow['bdBDID'];
+
+        /* Deleting record from Borrow table */
+        $borrowSQL = "DELETE FROM Borrow WHERE bID='$borrowDetailsID';";
+
+        $borrowResult = mysqli_query($databaseConn, $borrowSQL);
+
+        /* Deleting record from LibrarianManageBorrowDetails table */
+        $LMBDSQL = "DELETE FROM LibrarianManageBorrowDetails WHERE bID='$borrowDetailsID';";
+
+        $LMBDResult = mysqli_query($databaseConn, $LMBDSQL);
+
+        /* Deleting record from BorrowDetails table */
+        $borrowDetailsSQL = "DELETE FROM BorrowDetails WHERE bID='$borrowDetailsID';";
+
+        $borrowDetailsResult = mysqli_query($databaseConn, $borrowDetailsSQL);
+      }
+
     /* Deleting record from LibrarianManageBook table */
-    $LMBSQL = "DELETE FROM LibrarianManageBook WHERE bISBN='$ISBN';";
+    $LMBSQL = "DELETE FROM LibrarianManageBook WHERE bISBN='.$ISBN.';";
 
     $LMBResult = mysqli_query($databaseConn, $LMBSQL);
 
-    /* Retrieving BorrowDetails ID from the Borrow table */
-    $borrowDetailsIDSQL = "SELECT bID FROM BorrowDetails WHERE bISBN = '$ISBN';";
-
-    $borrowDetailsIDResult = mysqli_query($databaseConn, $borrowDetailsIDSQL);
-
-    $borrowDetailsID = "";
-
-    while($borrowDetailsIDRow = mysqli_fetch_array($borrowDetailsIDResult)){
-
-      $borrowDetailsID = $borrowDetailsIDRow['bID'];
-
-      /* Deleting record from Borrow table */
-      $borrowSQL = "DELETE FROM Borrow WHERE bID='$borrowDetailsID';";
-
-      $borrowResult = mysqli_query($databaseConn, $borrowSQL);
-
-      /* Deleting record from LibrarianManageBorrowDetails table */
-      $LMBDSQL = "DELETE FROM LibrarianManageBorrowDetails WHERE bID='$borrowDetailsID';";
-
-      $LMBDResult = mysqli_query($databaseConn, $LMBDSQL);
-
-      /* Deleting record from BorrowDetails table */
-      $borrowDetailsSQL = "DELETE FROM BorrowDetails WHERE bID='$borrowDetailsID';";
-
-      $borrowDetailsResult = mysqli_query($databaseConn, $borrowDetailsSQL);
-    }
-
-
+    /* Deleting record from Book table */
+    $bookSQL = "DELETE FROM Book WHERE ISBN='.$ISBN.';";
+echo $ISBN;
+    $bookResult = mysqli_query($databaseConn, $bookSQL);
 
     ?>
       <script>
@@ -59,7 +57,7 @@
     <?php
 
     echo "<script> location.href='librarianDashboard.php'; </script>";
-    exit;
+  //  exit;
 
     //header("Location: librarianDashboard.php");
 
